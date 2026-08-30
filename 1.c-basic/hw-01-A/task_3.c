80b34da49f1cf565571a90537c0f63677155d1ce
tadodaho <27009683+tadodaho@users.noreply.github.com>
2026-08-30 17:49:12 +0300

    root

---
+++ 1.c-basic/hw-01-A/task_3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+
+int main(void)
+{
+    printf(
+        "     *\n"
+        "    ***\n"
+        "   *****\n"
+        "  *******\n"
+        "   HHOHH\n"
+        "   ZZZZZ\n"
+    );
+    return 0;
+}
